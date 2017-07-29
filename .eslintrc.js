aee70774a45f4758df056781122af994a5823e03
The Zimmee <thezimmee@gmail.com>
2017-07-29 00:50:56 -0600

    Feature: Lint and format all files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	extends: 'eslint:recommended',
+	env: {
+		browser: true,
+	},
+	plugins: [
+		'html' // Required to lint *.vue files.
+	],
+	// Add custom rules here.
+	rules: {
+		// Require semicolons.
+		semi: ['warn', 'always'],
+		// Disallow multiple spaces.
+		'no-multi-spaces': ['warn', {ignoreEOLComments: true}],
+		// No trailing spaces.
+		'no-trailing-spaces': ['warn'],
+		// Allow debugger during development.
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+	}
+};
